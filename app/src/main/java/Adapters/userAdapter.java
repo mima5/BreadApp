fdf468317afcdcc2a51e7cc22dd8206ff8da37de
palguy <goooldfish3@gmail.com>
2018-10-25 10:39:03 +0300

    add edit user

--- app/src/main/java/Adapters/userAdapter.java
+++ app/src/main/java/Adapters/userAdapter.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.example.android.bread.AddUser;
 import com.example.android.bread.Models.User;
 import com.example.android.bread.R;
 import com.example.android.bread.ShowUseerDetails;
@@ -20,50 +21,46 @@ import io.realm.RealmResults;
 public class userAdapter extends RecyclerView.Adapter<userAdapter.ViewHolder> {
 
 
-        Context _ctx;
-        RealmResults<User> userList;
-        Realm realm;
+    Context _ctx;
+    RealmResults<User> userList;
+    Realm realm;
 
-        public userAdapter(RealmResults<User> userList, Context ctx) {
-            _ctx = ctx;
-            this.userList = userList;
-            this.realm = Realm.getDefaultInstance();
-        }
+    public userAdapter(RealmResults<User> userList, Context ctx) {
+        _ctx = ctx;
+        this.userList = userList;
+        this.realm = Realm.getDefaultInstance();
+    }
 
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-            View row = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_user, parent, false);
-            ViewHolder holder = new ViewHolder(row);
-            return holder;
-        }
+        View row = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_user, parent, false);
+        ViewHolder holder = new ViewHolder(row);
+        return holder;
+    }
 
-        @Override
-        public void onBindViewHolder(ViewHolder holder, final int position) {
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
 
-            final User item = userList.get(position);
+        final User item = userList.get(position);
 
 
-            holder.tv_userName.setText(item.getName());
-            holder.tv_userFlourAmount.setText(item.getFlourAmount()+"");
+        holder.tv_userName.setText(item.getName());
+        holder.tv_userFlourAmount.setText(item.getFlourAmount() + "");
 //            Toast.makeText(_ctx, "---- waw ----", Toast.LENGTH_SHORT).show();
 
 
-            holder.rlout.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+        holder.rlout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-                    Intent intent = new Intent(_ctx, ShowUseerDetails.class);
-                    intent.putExtra("id", item.getId());
-                    _ctx.startActivity(intent);
+                Intent intent = new Intent(_ctx, ShowUseerDetails.class);
+                intent.putExtra("id", item.getId());
+                _ctx.startActivity(intent);
 
 
-//                Intent i = new Intent(_ctx, EnterProductActivity.class);
-//                i.putExtra("id", item.getPt_id());
-//                // _ctx.startActivityForResult(i, 1);
-//                ((Activity) _ctx).startActivityForResult(i, 1);
-                }
-            });
+            }
+        });
 
 
         holder.img_delete.setOnClickListener(new View.OnClickListener() {
@@ -79,42 +76,54 @@ public class userAdapter extends RecyclerView.Adapter<userAdapter.ViewHolder> {
                 notifyDataSetChanged();
                 notifyItemRemoved(position);
 
-                notifyItemRangeChanged(position,userList.size());
+                notifyItemRangeChanged(position, userList.size());
             }
         });
 
-        }
+
+        holder.img_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(_ctx, AddUser.class);
+                intent.putExtra("id", item.getId());
+                _ctx.startActivity(intent);
+
+            }
+        });
+
+    }
 
 
-        @Override
-        public int getItemCount() {
-            return userList.size();
-        }
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
 
 
-        public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
-            private final TextView tv_userName;
-            private final TextView tv_userFlourAmount;
-            private final ImageView img_delete;
-            private final ImageView img_edit;
+        private final TextView tv_userName;
+        private final TextView tv_userFlourAmount;
+        private final ImageView img_delete;
+        private final ImageView img_edit;
 
-            private final RelativeLayout rlout;
+        private final RelativeLayout rlout;
 
 
-            public ViewHolder(View itemView) {
-                super(itemView);
+        public ViewHolder(View itemView) {
+            super(itemView);
 
-                tv_userName = (TextView) itemView.findViewById(R.id.tv_userName);
-                tv_userFlourAmount = (TextView) itemView.findViewById(R.id.tv_userFlourAmount);
-                img_delete = (ImageView) itemView.findViewById(R.id.img_delete);
-                img_edit = (ImageView) itemView.findViewById(R.id.img_edit);
-                rlout = (RelativeLayout) itemView.findViewById(R.id.rlout);
+            tv_userName = (TextView) itemView.findViewById(R.id.tv_userName);
+            tv_userFlourAmount = (TextView) itemView.findViewById(R.id.tv_userFlourAmount);
+            img_delete = (ImageView) itemView.findViewById(R.id.img_delete);
+            img_edit = (ImageView) itemView.findViewById(R.id.img_edit);
+            rlout = (RelativeLayout) itemView.findViewById(R.id.rlout);
 
 //
 
-            }
         }
-
-
     }
+
+
+}
